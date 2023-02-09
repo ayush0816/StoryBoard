c984af3951cd51d822c4a254b0f44adce9499c2e
ayush0816 <75437420+ayush0816@users.noreply.github.com>
2023-02-09 20:13:03 +0530

    final

--- routes/stories.js
+++ routes/stories.js
@@ -35,26 +35,26 @@ router.get("/", ensureAuth, async (req, res) => {
   }
 });
 
-// router.get("/:id", ensureAuth, async (req, res) => {
-//   try {
-//     let story = await Story.findById(req.params.id).populate("user").lean();
+router.get("/:id", ensureAuth, async (req, res) => {
+  try {
+    let story = await Story.findById(req.params.id).populate("user").lean();
 
-//     if (!story) {
-//       return res.render("error/404");
-//     }
+    if (!story) {
+      return res.render("error/404");
+    }
 
-//     if (story.user._id != req.user.id && story.status == "private") {
-//       res.render("error/404");
-//     } else {
-//       res.render("stories/show", {
-//         story,
-//       });
-//     }
-//   } catch (err) {
-//     console.error(err);
-//     res.render("error/404");
-//   }
-// });
+    if (story.user._id != req.user.id && story.status == "private") {
+      res.render("error/404");
+    } else {
+      res.render("stories/show", {
+        story,
+      });
+    }
+  } catch (err) {
+    console.error(err);
+    res.render("error/404");
+  }
+});
 
 router.get("/edit/:id", ensureAuth, async (req, res) => {
   try {
@@ -103,58 +103,58 @@ router.put("/:id", ensureAuth, async (req, res) => {
   }
 });
 
-// router.delete("/:id", ensureAuth, async (req, res) => {
-//   try {
-//     let story = await Story.findById(req.params.id).lean();
+router.delete("/:id", ensureAuth, async (req, res) => {
+  try {
+    let story = await Story.findById(req.params.id).lean();
 
-//     if (!story) {
-//       return res.render("error/404");
-//     }
+    if (!story) {
+      return res.render("error/404");
+    }
 
-//     if (story.user != req.user.id) {
-//       res.redirect("/stories");
-//     } else {
-//       await Story.remove({ _id: req.params.id });
-//       res.redirect("/dashboard");
-//     }
-//   } catch (err) {
-//     console.error(err);
-//     return res.render("error/500");
-//   }
-// });
+    if (story.user != req.user.id) {
+      res.redirect("/stories");
+    } else {
+      await Story.remove({ _id: req.params.id });
+      res.redirect("/dashboard");
+    }
+  } catch (err) {
+    console.error(err);
+    return res.render("error/500");
+  }
+});
 
-// router.get("/user/:userId", ensureAuth, async (req, res) => {
-//   try {
-//     const stories = await Story.find({
-//       user: req.params.userId,
-//       status: "public",
-//     })
-//       .populate("user")
-//       .lean();
+router.get("/user/:userId", ensureAuth, async (req, res) => {
+  try {
+    const stories = await Story.find({
+      user: req.params.userId,
+      status: "public",
+    })
+      .populate("user")
+      .lean();
 
-//     res.render("stories/index", {
-//       stories,
-//     });
-//   } catch (err) {
-//     console.error(err);
-//     res.render("error/500");
-//   }
-// });
+    res.render("stories/index", {
+      stories,
+    });
+  } catch (err) {
+    console.error(err);
+    res.render("error/500");
+  }
+});
 
-// router.get("/search/:query", ensureAuth, async (req, res) => {
-//   try {
-//     const stories = await Story.find({
-//       title: new RegExp(req.query.query, "i"),
-//       status: "public",
-//     })
-//       .populate("user")
-//       .sort({ createdAt: "desc" })
-//       .lean();
-//     res.render("stories/index", { stories });
-//   } catch (err) {
-//     console.log(err);
-//     res.render("error/404");
-//   }
-// });
+router.get("/search/:query", ensureAuth, async (req, res) => {
+  try {
+    const stories = await Story.find({
+      title: new RegExp(req.query.query, "i"),
+      status: "public",
+    })
+      .populate("user")
+      .sort({ createdAt: "desc" })
+      .lean();
+    res.render("stories/index", { stories });
+  } catch (err) {
+    console.log(err);
+    res.render("error/404");
+  }
+});
 
 module.exports = router;
